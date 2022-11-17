28d02a26b80abc8c7dd6d849aa6df879a8244609
Mario Posada <marioposadamiranda@gmail.com>
2022-11-16 23:04:57 -0300

    Add Input

--- src/screens/authnavigation/login1/index.js
+++ src/screens/authnavigation/login1/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { styles } from './styles';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Input } from 'react-native';
 import { colors } from '../../../constants/theme/index';
 
 const LogIn1 = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
+            <Input placeholder="Email" />
+            <Text style={styles.title} > LogIn </Text>
+            <Text style={styles.title} > LogIn </Text>
             <Text style={styles.title} > LogIn </Text>
             <Text style={styles.text} onPress={() => navigation.navigate('SignUp1')}>Don't have an account? </Text>
         </View>
--- src/screens/authnavigation/onboarding1/index.js
+++ src/screens/authnavigation/onboarding1/index.js
@@ -6,8 +6,11 @@ import { colors } from "../../../constants/theme/index";
 const OnBoarding1 = ({ navigation }) => {
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>OnBoarding1</Text>
-      <Text style={styles.text} onPress={()=> navigation.navigate('LogIn1')} > Don't have an account? </Text>
+      <Text style={styles.title}>OnBoarding1</Text>
+      <Text style={styles.text} onPress={() => navigation.navigate("LogIn1")}>
+        {" "}
+        Don't have an account?{" "}
+      </Text>
     </View>
   );
 };
