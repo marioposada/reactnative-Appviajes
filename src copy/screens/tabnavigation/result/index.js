d91ec35523e6ca04f240b0ca68cbecd2b104d599
Mario Posada <marioposadamiranda@gmail.com>
2022-11-19 15:24:37 -0300

    Add BottomBar

---
+++ src copy/screens/tabnavigation/result/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import {styles} from './styles'
+
+
+const ResultScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Result Screen</Text>
+        </View>
+    )
+}
+
+export default ResultScreen;
---
+++ src/screens/tabnavigation/map/styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+import {colors} from '../../../constants/theme';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.white,
+    },
+});
+
+
