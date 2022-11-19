a82fd998c1db768fa35a419127f720d35f272f24
Mario Posada <marioposadamiranda@gmail.com>
2022-11-19 16:04:17 -0300

    Delete some

--- src copy/screens/tabnavigation/index.js
+++
@@ -1,8 +0,0 @@
-export {default as Home} from './home'
-export {default as Ticket} from './ticket'
-export {default as Result} from './result'
-export {default as Map} from './map'
-export {default as Checkout} from './checkout'
-export {default as Alert}  from './alert'
-export {default as Transport} from './transport'
-
