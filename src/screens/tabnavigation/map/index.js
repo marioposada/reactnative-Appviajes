1bdc8263d21b52d772e159ed53b82af3689dbf15
Lucas Tabare <lucas.tabare@gmail.com>
2022-12-03 21:04:38 -0300

    Maps Screen

--- src/screens/tabnavigation/map/index.js
+++ src/screens/tabnavigation/map/index.js
@@ -1,24 +1,50 @@
-import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import * as React from "react";
+
+import { From, To, Transport } from "../../../components";
+import MapView, { Marker, Polyline } from "react-native-maps";
 import { Text, TouchableOpacity, View } from "react-native";
 
-import { GOOGLE_MAPS_KEY } from "@env";
+import FlagFinish from "../../../../assets/finish-flag.png";
+import FlagInit from "../../../../assets/init-flag.png";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 import MapViewDirections from "react-native-maps-directions";
-import React from "react";
 import { styles } from "./styles";
+import { useWindowDimensions } from "react-native";
 
 const MapScreen = ({ navigation }) => {
+  const { width, height } = useWindowDimensions();
+
   const [origin, setOrigin] = React.useState({
     latitude: -31.417,
     longitude: -64.183,
   });
 
   const [destination, setDestination] = React.useState({
-    latitude: 33.753746,
-    longitude: -84.38633,
+    latitude: -31.3,
+    longitude: -64.3,
   });
 
+  React.useEffect(() => {
+    getLocation;
+  }, []);
+
+  async function getLocation() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      alert("Permission denied");
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    const current = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    };
+    setOrigin(current);
+  }
+
   const handlePurchase = () => {
-    console.warn("tocado");
+    console.warn(origin.formatted_address);
   };
 
   return (
@@ -35,12 +61,13 @@ const MapScreen = ({ navigation }) => {
         <Marker
           draggable
           coordinate={origin}
-          image={carImage}
+          image={FlagInit}
           onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}
         />
         <Marker
           draggable
           coordinate={destination}
+          image={FlagFinish}
           onDragEnd={(direction) =>
             setDestination(direction.nativeEvent.coordinate)
           }
@@ -48,7 +75,7 @@ const MapScreen = ({ navigation }) => {
         <MapViewDirections
           origin={origin}
           destination={destination}
-          apikey={GOOGLE_MAPS_KEY}
+          apikey={GOOGLE_MAPS_APIKEY}
           strokeColor="black"
           strokeWidth={5}
         />
@@ -58,9 +85,12 @@ const MapScreen = ({ navigation }) => {
           strokeWidth={8}
         /> */}
       </MapView>
-
+      <View style={[styles.content, { width: width }]}>
+        <From text={origin} />
+        <To text={destination} />
+      </View>
       <TouchableOpacity style={styles.buttom} onPress={handlePurchase}>
-        <Text style={styles.buttomTitle}>Registrate</Text>
+        <Text style={styles.buttomTitle}>Aceptar</Text>
       </TouchableOpacity>
     </View>
   );
